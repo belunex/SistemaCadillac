63f85afab372ee89df40053ed2077e38192b62a2
belunex <rojas.anahi.9190@gmail.com>
2023-09-12 19:59:40 -0400

    modificacion

--- application/controllers/administrador.php
+++ application/controllers/administrador.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed'); 
 
-class Estudiantelte extends CI_Controller {
+class Administrador extends CI_Controller {
     public function indexlte()
 	{	
 		$this->load->view('administrador/board/cabecera');
