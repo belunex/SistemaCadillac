ab1699fc15de8f5da30fb3f3544a1045387399c7
belunex <rojas.anahi.9190@gmail.com>
2023-10-04 17:23:34 -0400

    Adicion de la vista de cotizacion

---
+++ application/views/jefeVista/cotizacion.php
@@ -0,0 +1,77 @@
+<div class="container">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Cotización</h3>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="numeroTel">Nombre Completo:</label>
+                        <input type="name" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Fecha:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Telefono:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Importe:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Tiempo de trabajo:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="numeroTel">Detalle:</label>
+                        <input type="texttarea" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
