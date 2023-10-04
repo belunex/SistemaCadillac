f0e943b6340e9051d16ae8805a6f7a00bf68bf12
belunex <rojas.anahi.9190@gmail.com>
2023-10-04 17:23:58 -0400

    Adicion de la vista de recepcion

---
+++ application/views/jefeVista/recepcion.php
@@ -0,0 +1,76 @@
+<div class="container">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Cotización</h3>
+        </div>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-3">
+                    <div id="example1_filter" class="dataTables_filter">
+                        <label>Buscar:<input type="search" class="form-control form-control-sm" placeholder="" aria-controls="example1"></label>
+                    </div>
+                </div>
+                <div class="col-6">
+                    
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Fecha:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Telefono:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Importe:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    <div class="form-group">
+                        <label for="numeroTel">Tiempo de trabajo:</label>
+                        <input type="text" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    
+                </div>
+                <div class="col-6">
+                    <div class="form-group">
+                        <label for="numeroTel">Detalle:</label>
+                        <input type="texttarea" class="form-control" id="numCel" name="numCel">
+                    </div>
+                </div>
+                <div class="col-3">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
