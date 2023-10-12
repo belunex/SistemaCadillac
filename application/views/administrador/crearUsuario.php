cf2144eed927d1368042bdf2dcccac75a37e6328
belunex <rojas.anahi.9190@gmail.com>
2023-10-12 16:22:01 -0400

    Adicion del archivo formulario para crear usuarios

---
+++ application/views/administrador/crearUsuario.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title" style="text-align: center;">Creacion de Usuarios</h3>
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-12">
+                    <input type="text">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
