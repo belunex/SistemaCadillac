a1d8296b4db0febdb977461eb1162574c74c72af
belunex <rojas.anahi.9190@gmail.com>
2023-10-26 17:17:05 -0400

    modificacion de la vista para modificar la contraseña

--- application/views/forgot_password.php
+++ application/views/forgot_password.php
@@ -28,9 +28,8 @@
 		<div class="d-flex flex-column flex-root">
 			<!--begin::Login-->
 			<div class="login login-4 login-signin-on d-flex flex-row-fluid" id="kt_login">
-				<div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat" style="background-image: url('assets/media/bg/bg-3.jpg');">
+				<div class="d-flex flex-center flex-row-fluid bgi-size-cover bgi-position-top bgi-no-repeat" style="background-image: url('<?php echo base_url();?>/gentelella/assets/media/bg/bg-3.jpg');">
 					<div class="login-form text-center p-7 position-relative overflow-hidden">
-						<!--begin::Login Header-->
 						<div class="d-flex flex-center mb-15">
 							<a href="#">
 								<img src="<?php echo base_url();?>/gentelella/production/images/iconCad.png" class="max-h-75px" alt="" />
@@ -38,20 +37,23 @@
 						</div>
                         <div class="login-forgot">
 							<div class="mb-20">
-								<h3>Forgotten Password ?</h3>
-								<div class="text-muted font-weight-bold">Enter your email to reset your password</div>
+								<h3>Olvidaste tu contraseña ? Te ayudamos a restaurarla</h3>
+								<div class="text-muted font-weight-bold">Ingresa tu email para reestablecer tu contraseña!!</div>
 							</div>
-							<form class="form" id="kt_login_forgot_form">
+							<?php
+								echo form_open_multipart('usuario/validarIngresoUsuarios', array('id' => 'form1','class' => 'needs-validation', 'method' => 'post'));
+							?>	
 								<div class="form-group mb-10">
-									<input class="form-control form-control-solid h-auto py-4 px-8" type="text" placeholder="Email" name="email" autocomplete="off" />
+									<input class="form-control form-control-solid h-auto py-4 px-8" type="email" placeholder="Ingrese su correo electronico valido" name="correoElectronico" autocomplete="off" required=""/>
 								</div>
 								<div class="form-group d-flex flex-wrap flex-center mt-10">
-									<button id="kt_login_forgot_submit" style="border-color: #FF7B00; background-color: #FF7B00" class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Request</button>
-									<button id="kt_login_forgot_cancel" style="border-color: #7AB700; background-color: #7AB700" class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Cancel</button>
+									<button type="submit" id="kt_login_forgot_submit" style="border-color: #FF7B00; background-color: #FF7B00" class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2">Enviar</button>
+									<button type="button" id="kt_login_forgot_cancel" style="border-color: #7AB700; background-color: #7AB700" class="btn btn-primary font-weight-bold px-9 py-4 my-3 mx-2"><a href="<?php echo base_url();?>index.php/usuario/index">Cancelar</a></button>
 								</div>
-							</form>
+							<?php
+								echo form_close();
+							?>	
 						</div>
-						<!--end::Login forgot password form-->
 					</div>
 				</div>
 			</div>
